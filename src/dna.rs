0d883fcae1610ef768e776c96add7903172ddd97
Joe Fredette <jfredett@gmail.com>
2018-11-05 00:14:46 -0500

    Implement Iterator traits for DNA

--- src/dna.rs
+++ src/dna.rs
@@ -14,8 +14,7 @@ pub struct DNA {
     // and want to represent it
     unused_mask: u64,
     /// Index of the end of the strand
-    terminus_idx: usize
-
+    terminus_idx: usize,
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -146,6 +145,50 @@ impl DNA {
     }
 }
 
+
+pub struct DNAIterator {
+    dna: DNA,
+    idx: usize
+}
+
+impl Iterator for DNAIterator {
+    type Item = Nucleotide;
+    fn next(&mut self) -> Option<Nucleotide> {
+        // we want bitpairs
+        let mask = 0b11;
+        // XXX: 32 == number of nucleotides per block, probably refactor this to calculate
+        // from the type?
+        let block_idx = self.idx % 32;
+        // we select in groups of 2, so 2x gets us the bit number we want
+        let shift = 2 * (self.idx / 32);
+        let element = self.dna.content[block_idx];
+        self.idx += 1;
+        if block_idx == self.dna.terminus_idx && shift == (self.dna.get_shift() + 1) {
+            // we're on the last block and past the end of strand.
+            None
+        } else {
+            // Extract the bitpair using our mask, convert it to a nucleotide, ship it.
+            //
+            // Example w/ 8-bit block size (real thing is 64 bit)
+            // element = 0b10_01_11_10, shift = 2.
+            // (0b10_01_10_11 & (0b11 << 2)) >> 2            ==>
+            // (0b10_01_10_11 & (0b00_00_00_11_00)) >> 2     ==>
+            // (0b00_00_10_00) >> 2                          ==>
+            // (0b00_00_00_10)
+            Some(Nucleotide::from((element & (mask << shift)) >> shift))
+        }
+    }
+}
+
+impl IntoIterator for DNA {
+    type Item = Nucleotide;
+    type IntoIter = DNAIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        DNAIterator { dna: self, idx: 0 }
+    }
+
+}
 
 #[cfg(test)]
 mod tests {
@@ -348,6 +391,30 @@ mod benches {
                 dna_strand.add(Nucleotide::T); dna_strand.add(Nucleotide::A); dna_strand.add(Nucleotide::C); dna_strand.add(Nucleotide::G);
 
                 dna_strand.add(Nucleotide::T); dna_strand.add(Nucleotide::A); dna_strand.add(Nucleotide::C); dna_strand.add(Nucleotide::G);
+            });
+        }
+    }
+
+    mod iter {
+        use super::*;
+
+        #[bench]
+        fn iter_short(b: &mut Bencher) {
+            b.iter(|| {
+                let dna = black_box(DNA::from_str("GATTACA"));
+                for nucleotide in dna {
+                    black_box(nucleotide);
+                }
+            });
+        }
+
+        #[bench]
+        fn iter_long(b: &mut Bencher) {
+            b.iter(|| {
+                let dna = black_box(DNA::from_str("GATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACAGATTACA"));
+                for nucleotide in dna {
+                    black_box(nucleotide);
+                }
             });
         }
     }
